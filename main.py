c84fa8c0fde707c4f717aad9033702b01449338b
unknown <jeremy.greenwald@gmail.com>
2023-02-09 13:58:21 -0500

    reset

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
 import sys
 
-from schedule import display_schedule
-
 if __name__ == '__main__':
     print(sys.argv[1])
-    display_schedule(sys.argv[1])
--- schedule.py
+++ schedule.py
@@ -2,27 +2,3 @@ import datetime
 from csv import DictReader
 
 
-def display_schedule(input_file):
-    schedule = create_schedule(input_file)
-    print_schedule(schedule)
-
-def print_schedule(schedule):
-    print(schedule)
-
-
-def create_schedule(input_file):
-    movies = read_movie_file(input_file)
-    return []
-
-def read_movie_file(input_file):
-    with open(input_file) as f:
-        dict_reader = DictReader(f)
-        list_of_movies = list(dict_reader)
-        print(list_of_movies)
-        for movie in list_of_movies:
-            movie[" Release Year"] = int(movie[" Release Year"])
-            movie[" MPAA Rating"] = movie[" MPAA Rating"].strip(" ")
-            runtime = movie[" Run Time"].strip(" ")
-            movie[" Run Time"] = datetime.time(hour=int(runtime.split(':')[0]),
-                                               minute=int(runtime.split(':')[1]))
-    return list_of_movies
